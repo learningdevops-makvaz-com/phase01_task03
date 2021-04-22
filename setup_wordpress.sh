d03650680a1ff2c73d90da2be284497a54beba5d
Daniel Palacios <danpal@tuta.io>
2021-04-22 16:13:08 -0500

    Changed localhost for the VM's IP. Now Wordpress loads a theme.

--- setup_wordpress.sh
+++ setup_wordpress.sh
@@ -31,6 +31,6 @@ wget ${wp_cli_url}
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 cd /var/www/html/wordpress
-wp core install --url=localhost --title="Blogging Platform" --admin_user=admin --admin_email=danpal@example.com --admin_password="!2three456." --allow-root 
+wp core install --url="192.168.50.2" --title="Blogging Platform" --admin_user=admin --admin_email=danpal@example.com --admin_password="!2three456." --allow-root 
 
 wp theme install twentynineteen --activate --allow-root
