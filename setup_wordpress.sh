648404b13c509f56a54571a2c0515eae5c46034c
rojin <therojinebrahimi@gmail.com>
2021-05-02 01:15:03 +0430

    Add if to check wp file existance , database create, add -f to soft link, edit wp cli version and changed sudo mv to sudo cp

--- setup_wordpress.sh
+++ setup_wordpress.sh
@@ -20,7 +20,7 @@ debconf-set-selections <<< "mysql-server mysql-server/root_password_again passwo
 
 sudo mysql -u root -p$mysql_password << EOF
 CREATE DATABASE IF NOT EXISTS wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'wordpressuser'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON wordpress.* to wordpressuser@'localhost';
 FLUSH PRIVILEGES;
 EOF
@@ -31,9 +31,9 @@ echo "------------------ Installing PHP ------------------"
 sudo apt-get -y install php-fpm php-mysql
 
 echo "------------------ Configuring SSL Certificate ------------------"
-sudo mv /vagrant/nginx-selfsigned.crt /etc/ssl/certs/
-sudo mv /vagrant/nginx-selfsigned.key /etc/ssl/private/
-sudo mv /vagrant/dhparam.pem /etc/nginx/
+cp /vagrant/nginx-selfsigned.crt /etc/ssl/certs/
+cp /vagrant/nginx-selfsigned.key /etc/ssl/private/
+cp /vagrant/dhparam.pem /etc/nginx/
 
 echo "------------------ Creating Configuration Snippet ------------------"
 cp /vagrant/self-signed.conf /etc/nginx/snippets/
@@ -47,8 +47,11 @@ sudo apt-get -y install php-curl php-gd php-intl php-mbstring php-soap php-xml p
 sudo systemctl restart php7.4-fpm
 
 echo "------------------ Configuring Nginx ------------------"
-sudo mkdir /var/www/wordpress
-sudo chown -R www-data:www-data /var/www/wordpress
+WP_DIR=/var/www/wordpress
+if [ ! -d "$WP_DIR" ]; then
+    sudo mkdir /var/www/wordpress
+fi
+#sudo chown -R www-data:www-data /var/www/wordpress/
 
 echo "------------------ Nginx Configuration ------------------"
 cp /vagrant/nginx_conf /etc/nginx/sites-available/wordpress
@@ -58,7 +61,8 @@ cp /vagrant/index.html /var/www/wordpress/index.html
 echo "You Can Test The Index Page At 192.168.50.2"
 
 echo "------------------ Creating Soft Link ------------------"
-sudo ln -s /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/
+sudo ln -sf /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/
+
 
 echo "------------------ Testing Nginx ------------------"
 sudo nginx -t
@@ -78,20 +82,17 @@ tar xzvf wordpress-5.7.1.tar.gz
 sudo cp -a /tmp/wordpress/. /var/www/wordpress
 
 echo "------------------ Setting Up Privileges ------------------"
-sudo chown -R www-data:www-data /var/www/wordpress
+sudo chown -R www-data:www-data /var/www/wordpress/
 
 echo "------------------ Configuring WordPress ------------------"
 cp /vagrant/wp-config.php /var/www/wordpress/wp-config.php
 
 echo "------------------ Installing WordPress CLI ------------------"
-WP_CLI=wp-cli.phar
-if [[ ! -f "$WP_CLI" ]]; then
-    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-fi
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 php wp-cli.phar --info
-sudo chmod +x wp-cli.phar
+chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
-wp cli version
+wp cli version --allow-root
 
 cd /var/www/wordpress
 echo "------------------ Creating WordPress User ------------------"
