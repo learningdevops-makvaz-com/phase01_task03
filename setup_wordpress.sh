0d94ea836c0ea12d6540b42edcfcb1fa831b99da
Teerminuz <ck.andrew.ng@gmail.com>
2021-05-24 11:52:53 +0200

    added changes and php install

--- setup_wordpress.sh
+++ setup_wordpress.sh
@@ -5,12 +5,9 @@ echo 'This script should install and setup Wordpress'
 
 # Variables needed for configurations
 
-Dbname = "wordpress"
-Dbuser = "wpuser"
-Dbpass = "Techgrounds101"
-
-
-
+Dbname ='wordpress'
+Dbuser ='wpuser'
+Dbpass ='Techgrounds101'
 
 
 
@@ -25,16 +22,17 @@ sudo apt update
 sudo apt-get upgrade -y
 sudo ufw enable
 sudo install nginx -y
-sudo apt install mysql-server -y
+sudo apt install mysql-server -y 
+Sudo apt install php7.4 7.4-fpm php7.4 7.4-mysql php7.4-curl php7.4-gd php7.4-intl php7.4-mbstring php7.4-soap php7.4-xml php7.4-xmlrpc php7.4-zip
 
-#installing nginx
+#configure firewall nginx
 sudo ufw allow 'Nginx HTTP'
 
 # install and configure mysql-server
 sudo mysql -e "CREATE DATABASE $Dbname DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
 sudo mysql -e "CREATE USER '$Dbuser'@'localhost' IDENTIFIED BY '$Dbpass';"
 sudo mysql -e "GRANT ALL ON $Dbname.* TO '$Dbuser'@'localhost';"
-sudo mysql -e "FLUSH PRIVELEGES;"
+sudo mysql -e "FLUSH PRIVILEGES;"
 
 
 
