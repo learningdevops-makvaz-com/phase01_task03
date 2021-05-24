e1ebde622230e081ae96eed2e990ffa142412a93
Teerminuz <ck.andrew.ng@gmail.com>
2021-05-24 20:25:10 +0200

    fixed the EOF not calling the uri variable

--- setup_wordpress.sh
+++ setup_wordpress.sh
@@ -52,8 +52,7 @@ server {
 	index index.html index.htm index.php;
 
 	location / {
-		#try_files $uri $uri/ =404;
-		try_files $uri $uri/ index.php$is_args$args;
+		try_files $(uri) $(uri)/ index.php$(is_args)$(args);
 	}
 
 	location ~ \.php$ {
