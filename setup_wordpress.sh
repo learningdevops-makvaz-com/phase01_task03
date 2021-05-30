ecb2b7d3892e772d44efafd079a36df427e6279f
Teerminuz <ck.andrew.ng@gmail.com>
2021-05-30 08:35:07 +0200

    fixes to directory

--- setup_wordpress.sh
+++ setup_wordpress.sh
@@ -15,11 +15,6 @@ username=Teerminuz
 # WordPress should have already configured with theme twentynineteen and with created user admin with password !2three456..
 #####################
 
-sudo adduser $username
-echo "$Dbpass" | passwd "$username" --stdin
-sudo usermod -aG sudo $username
-su $username
-echo "$Dbpass"
 
 # install the needed packages
 sudo apt update
@@ -47,8 +42,8 @@ sudo wget https://wordpress.org/latest.tar.gz
 tar xzvf latest.tar.gz
 sudo cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php
 sudo mkdir /home/wordpress/www/
-sudo cp -a /tmp/wordpress/. /home/www/wordpress
-sudo chown -R www-data:www-data /home/www/wordpress
+sudo cp -a /tmp/wordpress/. /home/wordpress/www
+sudo chown -R www-data:www-data /home/wordpress/www
 
 cat << EOF > /etc/nginx/sites-available/wordpress
 server {
@@ -89,7 +84,7 @@ sudo systemctl restart nginx
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 sudo chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
-cd /home/www/wordpress
+cd /home/wordpress/www
 wp core install --url="10.0.2.15"  --title="Cybergamerz" --admin_user="admin" --admin_password="$Dbpass" --admin_email="test@test.nl"
 wp theme install twentyten
 wp theme activate twentyten
