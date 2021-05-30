7a713d73e07d3e3029372977a1acaf7e30ab509a
Aliaksei Karneyeu <aliaksei.karneyeu@openx.com>
2021-05-31 01:20:24 +0200

    stop executing on first error occurance.

--- setup_wordpress.sh
+++ setup_wordpress.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Please do not remove this line. This command tells bash to stop executing on first error. 
+set -e
 
+# Your code goes below ...
 echo 'This script should install and setup Wordpress'
