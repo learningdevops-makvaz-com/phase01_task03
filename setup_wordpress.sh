75b099c59841f27cd092f4adad70bcc7f7c2b9e3
bertei <bteisceira@gmail.com>
2022-01-11 12:26:07 -0300

    fixes

--- setup_wordpress.sh
+++ setup_wordpress.sh
@@ -18,11 +18,11 @@ sudo apt-get install mysql-server -y
 
 echo '---------- Create WordPress Database & User ----------'
 #debconf-set-selections lets you install mysql unattended, mysql asks for password, and we set the password
-mysql_pw='YourPassword123!'
+mysql_pw='Your Secure Password 123'
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password ${mysql_pw}"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${mysql_pw}"
 #then we create the database from wpDB.sql file
-sudo mysql -u root -p$mysql_pw < /vagrant/wpDB.sql
+sudo mysql -u root -p"$mysql_pw" < /vagrant/wpDB.sql
 
 echo '---------- Install PHP Packages ----------'
 sudo apt install php7.4-cli php7.4-fpm php7.4-mysql php7.4-json php7.4-opcache php7.4-mbstring php7.4-xml php7.4-gd php7.4-curl -y
@@ -38,37 +38,37 @@ sudo cp /vagrant/self-signed.conf /etc/nginx/snippets/
 sudo cp /vagrant/ssl-params.conf /etc/nginx/snippets/
 
 echo "------------------ WordPress Nginx Directory ------------------"
-WP_directory=/var/www/wordpress
+WP_directory='/var/www/word press'
 if [ ! -d "$WP_directory" ]; then
-    sudo mkdir ${WP_directory}
+    sudo mkdir "${WP_directory}"
 fi
 
 echo "------------------ Nginx WordpressSite File Configuration && Symbolic link ------------------"
 sudo cp /vagrant/WPNginxConfig.conf /etc/nginx/sites-available/wordpress
-sudo ln -s /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/
-sudo unlink /etc/nginx/sites-enabled/default
+sudo ln -sf /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/
+sudo unlink -f /etc/nginx/sites-enabled/default
 sudo nginx -t
 sudo systemctl restart nginx
 
 echo "------------------ WordPress Download & Extract ------------------"
 cd /tmp
-sudo wget https://wordpress.org/latest.tar.gz
+wget https://wordpress.org/latest.tar.gz
 sudo tar xvf latest.tar.gz
 
 echo "------------------ WordPress Privileges Setup && Wordpress PHP Configuration ------------------"
-sudo cp -a /tmp/wordpress/* ${WP_directory}
-sudo chown -R www-data:www-data ${WP_directory}
-sudo cp /vagrant/wp-config.php /var/www/wordpress/wp-config.php
+sudo cp -a /tmp/wordpress/* "${WP_directory}"
+sudo chown -R www-data:www-data "${WP_directory}"
+sudo cp /vagrant/wp-config.php "${WP_directory}"/wp-config.php
 
 echo "------------------  WordPress CLI Installation ------------------"
 #curl -O, command that lets you download a file and flag -O saves the file with the original name
-sudo curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 sudo chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 
 echo "------------------  WordPress User && Wordpress Theme ------------------"
-cd /var/www/wordpress
-sudo wp core install --url="192.168.50.2" --title="WordPress Setup" --admin_user=admin --admin_email=admin123@admin.com --admin_password="!2three456." --allow-root
-sudo wp theme install "twentynineteen" --activate --allow-root
+cd "${WP_directory}"
+wp core install --url="192.168.50.2" --title="WordPress Setup" --admin_user=admin --admin_email=admin123@admin.com --admin_password="!2three456." --allow-root
+wp theme install "twentynineteen" --activate --allow-root
 
 sudo apt-get install w3m -y
